f4352060ee491f1263bf8f6f306cdd0da50373de
ouer1994 <861529741@qq.com>
2018-04-10 18:03:19 +0800

    seeding category and user data

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
 
@@ -7,10 +9,13 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        $faker = app(Faker\Generator::class);
+        $users = User::all()->pluck('id')->toArray();
+        $categories = Category::all()->pluck('id')->toArray();
+
+        $topics = factory(Topic::class)->times(100)->make()->each(function ($topic, $index) use ($faker, $users, $categories) {
+            $topic->user_id = $faker->randomElement($users);
+            $topic->category_id = $faker->randomElement($categories);
         });
 
         Topic::insert($topics->toArray());
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        $users = factory(\App\Models\User::class)->times(10)->make()->each(function ($user, $index) use ($faker, $avatars) {
+            $user->avatar = $faker->randomElement($avatars);
+        });
+
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+        DB::table('users')->insert($user_array);
+
+        $user1 = User::find(1);
+        $user1->name = 'kangda';
+        $user1->email = '1@qq.com';
+        $user1->save();
+
+    }
+}
