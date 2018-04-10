7e47ff0d17b0c26e3ed375b8047abd14f0b735be
ouer1994 <861529741@qq.com>
2018-04-10 17:40:16 +0800

    add topics

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,4 @@ $this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 // UsersController
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
