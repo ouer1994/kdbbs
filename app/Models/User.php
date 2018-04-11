25711bbc4eccc3f6469c6fcc774318937d76c65c
ouer1994 <861529741@qq.com>
2018-04-11 23:16:10 +0800

    Notify author when topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if (Auth::id() == $this->id) {
+            return;
+        }
+
+        $this->increment('notification_count', 1);
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -40,5 +54,13 @@ class User extends Authenticatable
     public function replies()
     {
         return $this->hasMany(Reply::class, 'user_id', 'id');
+    }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+
+        $this->unreadNotifications->markAsRead();
     }
 }
